59a6c0ed114634fdd23b027b80c766669fa267a0
Cherry3521 <anikaalisha91@gmail.com>
2026-03-06 12:18:11 +0600

    Add files via upload

---
+++ distinctnumber.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    int count = 1;
+
+    for(int i = 1; i < n; i++) {
+        if(v[i] != v[i-1]) {
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
